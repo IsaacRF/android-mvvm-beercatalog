a34f7aa4fca8a46fe41a28450be9a4a264270053
Isaac R.F <isaacrf239@gmail.com>
2020-08-20 09:08:13 +0200

    Separates recycler view adapter into its own class

--- app/src/main/java/com/isaacrf/android_base_app/features/beer_list/ui/BeerListActivity.kt
+++ app/src/main/java/com/isaacrf/android_base_app/features/beer_list/ui/BeerListActivity.kt
@@ -97,87 +97,12 @@ class BeerListActivity : AppCompatActivity() {
 
     private fun setupRecyclerView(recyclerView: RecyclerView, items: List<Beer>) {
         recyclerView.adapter =
-            SimpleItemRecyclerViewAdapter(
+            BeerListItemViewAdapter(
                 this,
                 items,
                 twoPane
             )
     }
 
-    class SimpleItemRecyclerViewAdapter(private val parentActivity: BeerListActivity,
-                                        private val values: List<Beer>,
-                                        private val twoPane: Boolean) :
-            RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
 
-        private val onClickListener: View.OnClickListener
-
-        init {
-            onClickListener = View.OnClickListener { v ->
-                val item = v.tag as Beer
-                if (twoPane) {
-                    val fragment = BeerDetailFragment()
-                        .apply {
-                        arguments = Bundle().apply {
-                            putString(BeerDetailFragment.ARG_ITEM_ID, item.id.toString())
-                        }
-                    }
-                    parentActivity.supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.item_detail_container, fragment)
-                            .commit()
-                } else {
-                    val intent = Intent(v.context, BeerDetailActivity::class.java).apply {
-                        putExtra(BeerDetailFragment.ARG_ITEM_ID, item.id.toString())
-                    }
-                    v.context.startActivity(intent)
-                }
-            }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.beer_list_item_view, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val item = values[position]
-            holder.txtBeerName.text = item.name
-            holder.txtBeerTagline.text = item.tagline
-            holder.txtAlcoholByVolume.text = item.alcoholByVolume.toString()
-
-            /*
-                This library is also developed by me, and automatically handles image decoding
-                and caching (https://github.com/IsaacRF/EpicBitmapRenderer)
-             */
-            EpicBitmapRenderer.decodeBitmapFromUrl(item.imageUrl,
-                holder.imgBeer.width,
-                holder.imgBeer.height,
-                { holder.imgBeer.setImageBitmap(it) },
-                { Log.d("BeerListActivity", "Failed to decode image ${item.imageUrl}") })
-
-            if (!item.available) {
-                holder.itemView.setBackgroundColor(
-                    ContextCompat.getColor(
-                        this.parentActivity,
-                        R.color.colorBeerNotAvailable
-                    )
-                )
-            }
-
-            with(holder.itemView) {
-                tag = item
-                setOnClickListener(onClickListener)
-            }
-        }
-
-        override fun getItemCount() = values.size
-
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val txtBeerName: TextView = view.findViewById(R.id.txtBeerName)
-            val txtBeerTagline: TextView = view.findViewById(R.id.txtBeerTagline)
-            val txtAlcoholByVolume: TextView = view.findViewById(R.id.txtAlcoholByVolume)
-            val imgBeer: ImageView = view.findViewById(R.id.imgBeer)
-        }
-    }
 }
--- app/src/main/java/com/isaacrf/android_base_app/features/beer_list/ui/BeerListItemViewAdapter.kt
+++ app/src/main/java/com/isaacrf/android_base_app/features/beer_list/ui/BeerListItemViewAdapter.kt
@@ -1,96 +1,99 @@
 package com.isaacrf.android_base_app.features.beer_list.ui
 
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.isaacrf.epicbitmaprenderer.core.EpicBitmapRenderer
 import com.isaacrf.android_base_app.R
+import com.isaacrf.android_base_app.features.beer_detail.ui.BeerDetailActivity
+import com.isaacrf.android_base_app.features.beer_detail.ui.BeerDetailFragment
 import com.isaacrf.android_base_app.features.beer_list.models.Beer
 import kotlinx.android.synthetic.main.beer_list_item_view.view.*
 
 /**
  * UI Adapter for Beer List RecyclerView items
  */
-class BeerListItemViewAdapter(
-    private val beers: List<Beer>,
-    private val onBeerListener: OnBeerListener
-) : RecyclerView.Adapter<BeerListItemViewAdapter.ViewHolder>() {
+class BeerListItemViewAdapter(private val parentActivity: BeerListActivity,
+                                    private val values: List<Beer>,
+                                    private val twoPane: Boolean) :
+    RecyclerView.Adapter<BeerListItemViewAdapter.ViewHolder>() {
 
-    private lateinit var context: Context
+    private val onClickListener: View.OnClickListener
 
-    /**
-     * Listener to override click and long click actions
-     */
-    interface OnBeerListener {
-        fun onBeerClick(beer: Beer)
-        fun onBeerLongClick(beer: Beer)
-    }
-
-    /**
-     * Provide a reference to the views for each data item
-     */
-    class ViewHolder(
-        view: View,
-        private val onBeerListener: OnBeerListener,
-        private val beers: List<Beer>
-    ) :
-        RecyclerView.ViewHolder(view), View.OnClickListener, View.OnLongClickListener {
-
-        init {
-            view.setOnClickListener(this)
-            view.setOnLongClickListener(this)
-        }
-
-        override fun onClick(view: View) {
-            onBeerListener.onBeerClick(beers[adapterPosition])
-        }
-
-        override fun onLongClick(view: View): Boolean {
-            onBeerListener.onBeerLongClick(beers[adapterPosition])
-            return true
+    init {
+        onClickListener = View.OnClickListener { v ->
+            val item = v.tag as Beer
+            if (twoPane) {
+                val fragment = BeerDetailFragment()
+                    .apply {
+                        arguments = Bundle().apply {
+                            putString(BeerDetailFragment.ARG_ITEM_ID, item.id.toString())
+                        }
+                    }
+                parentActivity.supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.item_detail_container, fragment)
+                    .commit()
+            } else {
+                val intent = Intent(v.context, BeerDetailActivity::class.java).apply {
+                    putExtra(BeerDetailFragment.ARG_ITEM_ID, item.id.toString())
+                }
+                v.context.startActivity(intent)
+            }
         }
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ViewHolder {
-        context = parent.context
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.beer_list_item_view, parent, false) as View
-        return ViewHolder(view, onBeerListener, beers)
+            .inflate(R.layout.beer_list_item_view, parent, false)
+        return ViewHolder(view)
     }
-
-    override fun getItemCount(): Int = beers.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val beer = beers[position]
-        holder.itemView.txtBeerName.text = beer.name
-        holder.itemView.txtBeerTagline.text = beer.tagline
-        holder.itemView.txtAlcoholByVolume.text = beer.alcoholByVolume.toString()
+        val item = values[position]
+        holder.txtBeerName.text = item.name
+        holder.txtBeerTagline.text = item.tagline
+        holder.txtAlcoholByVolume.text = item.alcoholByVolume.toString()
 
         /*
             This library is also developed by me, and automatically handles image decoding
             and caching (https://github.com/IsaacRF/EpicBitmapRenderer)
          */
-        EpicBitmapRenderer.decodeBitmapFromUrl(beer.imageUrl,
-            holder.itemView.imgBeer.width,
-            holder.itemView.imgBeer.height,
-            { holder.itemView.imgBeer.setImageBitmap(it) },
-            { Log.d("BeerListActivity", "Failed to decode image ${beer.imageUrl}") })
+        EpicBitmapRenderer.decodeBitmapFromUrl(item.imageUrl,
+            holder.imgBeer.width,
+            holder.imgBeer.height,
+            { holder.imgBeer.setImageBitmap(it) },
+            { Log.d("BeerListActivity", "Failed to decode image ${item.imageUrl}") })
 
-        if (!beer.available) {
+        if (!item.available) {
             holder.itemView.setBackgroundColor(
                 ContextCompat.getColor(
-                    context,
+                    this.parentActivity,
                     R.color.colorBeerNotAvailable
                 )
             )
         }
+
+        with(holder.itemView) {
+            tag = item
+            setOnClickListener(onClickListener)
+        }
+    }
+
+    override fun getItemCount() = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtBeerName: TextView = view.findViewById(R.id.txtBeerName)
+        val txtBeerTagline: TextView = view.findViewById(R.id.txtBeerTagline)
+        val txtAlcoholByVolume: TextView = view.findViewById(R.id.txtAlcoholByVolume)
+        val imgBeer: ImageView = view.findViewById(R.id.imgBeer)
     }
 }
